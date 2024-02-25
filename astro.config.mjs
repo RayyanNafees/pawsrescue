ff1b308181995c73ddc09d324b96b6e84fcbcbe3
RayyanNafees <nafees.rayyan@gmail.com>
2024-02-25 23:36:13 +0530

    removed edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: 'hybrid',
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
 })
