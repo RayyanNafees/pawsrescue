f18f753b5d91d6a3e9f0c0a8a60cdceaccfa44ba
RayyanNafees <nafees.rayyan@gmail.com>
2024-02-25 23:24:55 +0530

    Lock file generated with netlify edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  output: "hybrid",
-  adapter: netlify()
-});
+  output: 'hybrid',
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
+})
